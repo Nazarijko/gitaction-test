6c9e7c08ee115a966e189f1937b89662264f67e8
Nazarijko <nazarijko4@gmail.com>
2023-03-08 12:02:12 +0200

    update main.py

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("Hello, GitAction!")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return 'Hello, world!'
+
+if __name__ == '__main__':
+    app.run()
